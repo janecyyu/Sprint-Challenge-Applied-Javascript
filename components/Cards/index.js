9bbbbfdb8e4bf607c5279ebbea70710526bec8b7
janecyyu <janecyyu@gmail.com>
2020-03-13 09:52:57 -0700

    add mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,8 @@
 // STEP 3: Create Article cards.
 // -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Send an HTTP GET request to the following address:
+// https://lambda-times-backend.herokuapp.com/articles
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +18,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response.data);
+    const parent = document.querySelector(".cards-container");
+    const node = [];
+    response.data.articles.node.forEach(e => node.push(e));
+    node.forEach(e => parent.append(CardMaker(e)));
+    node.forEach(e => console.log(e.authorPhoto));
+  });
+
+function CardMaker(article) {
+  //create elements
+  const cardContainer = document.createElement("div"),
+    title = document.createElement("div"),
+    authorContainer = document.createElement("div"),
+    imageContainer = document.createElement("div"),
+    image = document.createElement("img"),
+    name = document.createElement("span");
+
+  //append
+  cardContainer.append(title);
+  cardContainer.append(authorContainer);
+  authorContainer.append(imageContainer, name);
+  imageContainer.append(image);
+
+  //add class name
+  cardContainer.classList.add("card");
+  title.classList.add("headline");
+  authorContainer.classList.add("author");
+  imageContainer.classList.add("img-container");
+
+  //add content
+  title.textContent = article.headline;
+  image.src = article.authorPhoto;
+  name.textContent = article.authorName;
+
+  return cardContainer;
+}
